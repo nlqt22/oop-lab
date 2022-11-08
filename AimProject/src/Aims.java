73e972486d5ce25e190cae79d422ee4db6844714
tuannq2202 <80348326+tuannq2202@users.noreply.githubgit config --global user.name tuannq2202git config --global user.email 80348326+tuannq2202@users.noreply.github>
2022-11-08 17:14:32 +0700

    Lab02

--- AimProject/src/Aims.java
+++ AimProject/src/Aims.java
@@ -2,16 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Aims {
-    public static List<DigitalVideoDisc> itemsInStore() {
-        List<DigitalVideoDisc> store = new ArrayList<>();
+    private static List<DigitalVideoDisc> store;
+
+    public static void addToStore() {
+        store = new ArrayList<>();
         store.add(new DigitalVideoDisc("Lion King", "Animation", "Roger Allers", 87, 19.95f));
         store.add(new DigitalVideoDisc("Star Wars", "Science Fiction", "87", 87, 24.95f));
         store.add(new DigitalVideoDisc("Aladin", "Animation",19.95f));
-
-        return store;
+        return ;
     }
 
-    public static void showItemsInStore(List<DigitalVideoDisc> store) {
+    public static void showItemsInStore() {
         System.out.println("Available in Ti-do:");
         int index = 0;
         for(DigitalVideoDisc disc : store) {
@@ -19,13 +20,17 @@ public class Aims {
         }
     }
 
+//    public static DigitalVideoDisc findDvdByTitle(String title) {
+//        for
+//    }
+
     public static void showTotalCost(Cart cart) {
         System.out.print("Total : " + cart.totalCost() + "$");
     }
 
     public static void main(String[] args) {
-        List<DigitalVideoDisc> store = itemsInStore();
-        showItemsInStore(store);
+        addToStore();
+        showItemsInStore();
         Cart yourCart = new Cart();
         yourCart.removeDigitalVideoDisc(store.get(1));
 
